70d26af142668952c00ca27b0308932c400f3db0
Yiannis Simillides <ys1818@me-ys1818g4.me.ic.ac.uk>
2019-03-04 12:07:39 +0000

    fixed deploy

--- docs/make.jl
+++ docs/make.jl
@@ -6,13 +6,14 @@ makedocs(
     sitename = "MLJ",
     format = Documenter.HTML(),
     modules = [MLJ]
+)
 
+deploydocs(
+    repo = "https://github.com/ysimillides/MLJ.jl"
 )
 
 #    modules = [MLJ]
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "https://github.com/ysimillides/MLJ.jl"
-)=#
+
